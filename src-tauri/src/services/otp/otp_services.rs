7f47797a72494359a0c1c7720df5ce7eb06cf42c
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-05-08 01:35:07 +0530

    return the token and verify the otp

--- src-tauri/src/services/otp/otp_services.rs
+++ src-tauri/src/services/otp/otp_services.rs
@@ -1,6 +1,14 @@
-use super::modules::job::token_job_response::TokenJobResponse;
+
+
+use crate::modules::job::token_job_response::TokenJobResponse;
 
 pub async fn verify_otp(otp : &str) -> Result<TokenJobResponse , String >{
-    let base_url = std::env::var("API_BASE_URL");
-    
+    let base_url = std::env::var("API_BASE_URL").map_err(|_|"API ERROR NOT SET .env".to_string())?;
+    let url = format!("{}/verify_otp?otp={}" , base_url , otp);
+    let response = reqwest::get(&url).await.map_err(|e|e.to_string())?;
+    if !response.status().is_success(){
+        return Err("Invalid OTP".into());
+    }
+    let data:TokenJobResponse = response.json().await.map_err(|e|e.to_string())?;
+Ok(data)
 }

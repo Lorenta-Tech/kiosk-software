390706e01f9ce8bdbbebd5c18421beb6e86137ad
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-05-08 02:15:58 +0530

    ..

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// use crate::commands::otp::otp_commands;
+// use crate::commands::prin_job::job_commands;
 mod services;
 mod modules;
 mod commands;
-fn main() {
-    dotenvy::dotenv().ok();
-    tauri::Builder::default().invoke_handler(tauri::generate_handler![
-    
-    ])
 
-    kiosk_engg_lib::run()
+fn main() {
+    // Load .env
+    dotenvy::dotenv().ok();
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            commands::otp::otp_commands::verify_otp_commands,
+            commands::prin_job::job_commands::download_pdf_url_commands
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri app");
+
+    // If you REALLY need a custom loop
+    // kiosk_engg_lib::run();
 }
--- src-tauri/src/services/pdf_job/job_services.rs
+++ src-tauri/src/services/pdf_job/job_services.rs
@@ -9,7 +9,7 @@ use reqwest::Client;
 pub async fn dowload_pdf(url : &str , file_name : &str )-> Result<String , String >{
    let download_dir = std::env::var("DOWNLOAD_DIR").unwrap_or_else(|_| "./downloads".to_string());
 
-    fs::create_dir_all(&download_dir).map_err(|e|e.to_string());
+    let _ = fs::create_dir_all(&download_dir).map_err(|e|e.to_string());
     let full_path = format!("{}/{}" , download_dir , file_name);
     let client = Client::new();
     let bytes = client.get(url).send().await.map_err(|e|e.to_string())?.bytes().await.map_err(|e|e.to_string())?;

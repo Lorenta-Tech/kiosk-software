f7c14db073610c4d5bbc15f7ac555a8aedbd7fbe
suhas-developer07 <sharmasuhas22@gmail.com>
2026-05-13 22:47:56 +0530

    ..

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,39 +7,40 @@ mod commands;
 use tauri::{Manager, WindowEvent};
 
 fn main() {
+
     dotenvy::dotenv().ok();
 
     tauri::Builder::default()
+
         .setup(|app| {
 
             let window = app.get_webview_window("main").unwrap();
 
-            // Fullscreen
-            window.set_fullscreen(true).unwrap();
-
-            // Always on top
-            window.set_always_on_top(true).unwrap();
-
-            // Prevent resize
-            window.set_resizable(false).unwrap();
-
-            // Hide decorations/titlebar
             window.set_decorations(false).unwrap();
 
-            // Focus window
-            window.set_focus().unwrap();
+            window.set_resizable(false).unwrap();
+
+            window.set_always_on_top(true).unwrap();
+
+            window.set_position(
+                tauri::PhysicalPosition::new(0, 0)
+            ).unwrap();
+
+            window.set_size(
+                tauri::PhysicalSize::new(1080, 1920)
+            ).unwrap();
 
             Ok(())
         })
 
         .on_window_event(|window, event| {
+
             match event {
-                // Prevent close
+
                 WindowEvent::CloseRequested { api, .. } => {
                     api.prevent_close();
                 }
 
-                // Re-focus if user switches away
                 WindowEvent::Focused(false) => {
                     let _ = window.set_focus();
                 }
@@ -49,12 +50,18 @@ fn main() {
         })
 
         .invoke_handler(tauri::generate_handler![
+
             commands::otp::otp_commands::verify_otp_commands,
+
             commands::pdf_job::job_commands::download_pdf_url_commands,
+
             commands::print::print_commands::print_pdf_command,
+
             commands::network::network_commands::check_internet,
+
         ])
 
         .run(tauri::generate_context!())
+
         .expect("error while running tauri app");
 }
--- vite.config.ts
+++ vite.config.ts
@@ -12,19 +12,20 @@ export default defineConfig(async () => ({
     react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
+  base: "./",
+
   // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 1421,
     strictPort: true,
     host: host || false,
     hmr: host
       ? {
           protocol: "ws",
           host,
-          port: 1421,
+          port: 1422,
         }
       : undefined,
     watch: {
